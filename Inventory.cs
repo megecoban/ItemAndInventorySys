d08a41dacaaa01976663c0e72c885b85f1ebdf27
Ege <61700957+megecoban@users.noreply.github.com>
2024-03-21 01:26:33 +0300

    Add files via upload

---
+++ Inventory.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private List<InventoryItem> inventoryItems = new List<InventoryItem>();
+
+    public float TotalWeightOfInventory => GetInventoryWeight();
+    
+
+    public void AddItem(InventoryItem item)
+    {
+        for (int i = 0; i < inventoryItems.Count; i++)
+        {
+            if (inventoryItems[i].ID == item.ID)
+            {
+                inventoryItems[i].AddAmount(item.Amount);
+            }
+        }
+    }
+
+    public void RemoveItemAmount(int itemID, int amount)
+    {
+        for (int i = 0; i < inventoryItems.Count; i++)
+        {
+            if (inventoryItems[i].ID == itemID)
+            {
+                inventoryItems[i].RemoveAmount(amount);
+                break;
+            }
+        }
+    }
+
+    public InventoryItem RemoveItem(int itemID)
+    {
+        for (int i = 0; i < inventoryItems.Count; i++)
+        {
+            if (inventoryItems[i].ID == itemID)
+            {
+                InventoryItem temp = inventoryItems[i];
+                inventoryItems.RemoveAt(i);
+                return temp;
+            }
+        }
+
+        return null;
+    }
+
+    public List<InventoryItem> GetAllItems()
+    {
+        return inventoryItems;
+    }
+
+    public InventoryItem GetItemAt(int index)
+    {
+        return inventoryItems[index];
+    }
+
+    public float GetInventoryWeight()
+    {
+        float returnVal = 0f;
+
+        for(int i = 0;i < inventoryItems.Count;i++)
+        {
+            returnVal += inventoryItems[i].Weight * inventoryItems[i].Amount;
+        }
+
+        return returnVal;
+    }
+
+    public void CheckAndArrange()
+    {
+        for(int i = 0; i<inventoryItems.Count; i++)
+        {
+            for (int k = 0; k < inventoryItems.Count; k++)
+            {
+                if (i == k)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (inventoryItems[i].ID == inventoryItems[k].ID && i != k)
+                    {
+                        inventoryItems[i].AddAmount(inventoryItems[k].Amount);
+                        inventoryItems[k].RemoveAmount(inventoryItems[k].Amount);
+                    }
+                }
+            }
+        }
+
+        RemoveIfEmpty();
+    }
+
+    private void RemoveIfEmpty()
+    {
+        for(int i=inventoryItems.Count-1;i>=0;i--)
+        {
+            if(inventoryItems[i].Amount <= 0)
+            {
+                inventoryItems.RemoveAt(i);
+            }
+        }
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class Item
+{
+    private int id;
+    private string name;
+    private string description;
+    private float weight;
+
+    public int ID => id;
+    public string Name => name;
+    public string Description => description;
+    public float Weight => weight;
+
+    public Item(int id, string name, string desc, float weight)
+    {
+        this.id = id;
+        this.name = name;
+        this.description = desc;
+        this.weight = weight;
+    }
+
+    public Item GetAsItem()
+    {
+        return (Item)this;
+    }
+
+}
+
+public class InventoryItem : Item
+{
+    private int amount;
+
+    public int Amount => amount;
+
+
+    public InventoryItem(int ID, string name, string desc, float weight, int amount) : base (ID, name, desc, weight)
+    {
+        this.amount = Mathf.Abs(amount);
+    }
+
+    public InventoryItem(Item item, int amount) : base(item.ID, item.Name, item.Description, item.Weight)
+    {
+        this.amount = Mathf.Abs(amount);
+    }
+
+
+    public bool AddAmount(int amount = 1)
+    {
+        amount = Mathf.Abs(amount);
+
+        this.amount += amount;
+        return true;
+    }
+
+    public bool RemoveAmount(int amount = 1)
+    {
+        amount = Mathf.Abs(amount);
+
+        if (this.amount - amount >= 0)
+        {
+            this.amount -= amount;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public InventoryItem GetAsInventoryItem()
+    {
+        return (InventoryItem)this;
+    }
+}
+
+public class SaleItem : Item
+{
+    private float price;
+
+    public float Price => Mathf.Abs(price);
+
+
+    public SaleItem(int ID, string name, string desc, float weight, float price) : base (ID, name, desc, weight)
+    {
+        this.price = Mathf.Abs(price);
+    }
+
+    public SaleItem(Item item, float price): base(item.ID, item.Name, item.Description, item.Weight)
+    {
+        this.price = Mathf.Abs(price);
+    }
+
+
+    public SaleItem GetAsSaleItem()
+    {
+        return (SaleItem)this;
+    }
+
+    public InventoryItem Take(int amount = 1)
+    {
+        amount = Mathf.Abs(amount);
+        InventoryItem returnItem = new InventoryItem((Item)this, amount);
+        return returnItem;
+    }
+}
